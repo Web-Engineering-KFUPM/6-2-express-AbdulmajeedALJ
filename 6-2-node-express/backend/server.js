766dcbb2bee46d362a76c1dc818379e84ba3325b
AbdulmajeedALJ <s202261920@kfupm.edu.sa>
2025-11-10 09:21:02 +0300

    Finsished

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,10 +2,23 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
+
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
 
-{/*write code to define routes*/}
+
+app.use(cors());
 
 
-{/*write code to create server*/}
+app.get("/", (req, res) => {
+    res.send("Welcome to the Quotes API!");
+});
+
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+});
